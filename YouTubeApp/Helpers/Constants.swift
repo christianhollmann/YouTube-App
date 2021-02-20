87c0da35f9b3c1e56b43a36b560dd48fa793aeb1
christianhollmann <37487790+christianhollmann@users.noreply.github.com>
2021-02-20 12:35:39 +0100

    First Setup

---
+++ YouTubeApp/Helpers/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  YouTubeApp
+//
+//  Created by Christian Hollmann on 20.02.21.
+//
+
+import Foundation
+
+struct Constants {
+    
+    let API_KEY = "AIzaSyD-sDxx4PqICjd7rKNxaPmj1kG7WjsZvYM"
+}
---
+++ YouTubeApp/YouTubeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouTubeAppApp.swift
+//  YouTubeApp
+//
+//  Created by Christian Hollmann on 20.02.21.
+//
+
+import SwiftUI
+
+@main
+struct YouTubeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
